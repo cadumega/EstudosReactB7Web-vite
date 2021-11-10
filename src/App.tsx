424ac0c39dbdf2d3e2247041568f4e273287783c
cadumega <72901445+cadumega@users.noreply.github.com>
2021-11-10 14:59:27 -0300

    exercicioStateEventProps

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,45 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-    const [n, setN] = useState(0);
+    const [name, setName] = useState('');
+    const [sobrenome, setSobrenome] = useState('');
+    const [idade, setIdade] = useState('');
 
-        const handleMinus = () => {
-            setN (n -1)
-        }
-
-
-        const handleMore = () => {
-            setN (n +1)
-        }
+    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    }
+    const handleInput2 = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSobrenome(event.target.value);
+    }
+    const handleInput3 = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setIdade(event.target.value);
+    }
 
     return (
         <div>
-            <button onClick= {handleMinus}> - </button>
-            <div>{n}</div>
-            <button onClick= {handleMore}> + </button>
+            Nome:
+            <input type="text" value={name} onChange={handleInput} />
+            <br />
+            Sobrenome:
+            <input type="text" value={sobrenome} onChange={handleInput2} />
+            <br />
+            Idade:
+            <input type="text" value={idade} onChange={handleInput3} />
+            <hr />
+
+            Olá {name} {sobrenome}, tudo bem?
+            <br />
+            Você tem {idade} anos.
         </div>
     )
 }
 
 export default App;
 
-// Iremos criar um contador com incremento de +1 toda vez que clico.
-// Iremos fazer um botão de +1 e de -1.
-// Somente a div que é modificada.
+// Todo sistema irá usar algum tipo de input, no qual é um html em que o usuário irá poder inserir algum dado.
+// O js não permite alteração pois esta associado ao state. Preciso criar um evento e a função.
+// Preciso importar React por fora o type para input é React.changeEvent<HTMLInputElement , type para change event.
+// Ação que modifica várias outras alterações no meu sistema, por exemplo um cadastro do cliente.
+
+// Meu state esta armazenando o conteúdo de armazenagem, criamos um formulário com uma state específica. O valor que você digita esta sendo salvo, em que poderá utilizar como quiser com esses valores.
+// Associamos duplamente o state ao value, e associamos para alterar o valor.
