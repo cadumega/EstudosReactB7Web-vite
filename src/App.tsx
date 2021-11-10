13ea0706c93f4d7aee49d174bb598e52e6095698
cadumega <72901445+cadumega@users.noreply.github.com>
2021-11-09 22:38:31 -0300

    projetoContador-useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import { Header } from './components/Header'
-import { Photo } from './components/Photo';
+import { useState } from 'react';
 
 const App = () => {
+    const [n, setN] = useState(0);
+
+        const handleMinus = () => {
+            setN (n -1)
+        }
+
+
+        const handleMore = () => {
+            setN (n +1)
+        }
+
     return (
         <div>
-            <Header title="Este é um exemplo" />
-            <Header title="Exemplo 2" />
-            <Header />
-
-            Olá Mundo
-
-            <Photo legend="Google">
-                <img src="http://www.google.com.br/google.jpg" alt="" />
-            </Photo>
+            <button onClick= {handleMinus}> - </button>
+            <div>{n}</div>
+            <button onClick= {handleMore}> + </button>
         </div>
-    );
+    )
 }
 
 export default App;
 
-// Quando tenho um bloco de código grande e quero dar manutenção separada, crio um componente para ele. Ou para ser utilizado em vários locais, como exibir avisos específicos. Crio um componente e sempre que eu quiser exibir esse componente de aviso já esta criado.
-// 200 componentes, imagina a loucura que seria se tivesse em um único arquivo.
-// Criar pasta components
+// Iremos criar um contador com incremento de +1 toda vez que clico.
+// Iremos fazer um botão de +1 e de -1.
+// Somente a div que é modificada.
