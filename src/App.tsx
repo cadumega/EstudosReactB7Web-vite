9cbbba1733ba19061cfd444834b1a1c029ce5fd6
cadumega <72901445+cadumega@users.noreply.github.com>
2021-11-10 21:51:53 -0300

    Renderizar com condicional - eventPropsStates.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,43 @@
-import { Pessoa } from "./components/Pessoa"
+import {useState } from 'react';
 
 const App = () => {
-    let list = [
-        {name: 'Carlos', age:30},
-        {name: 'João', age:40},
-        {name: 'Pedro', age:50},
-        {name: 'Maria', age:10},
-        {name: 'Carol', age:25},
-    ]
+    const [show, setShow] = useState(false);
+
+    const handleClick = () => {
+        setShow( !show )
+        
+        // if(show) {
+        //     setShow(false);
+        // } else {
+        //     setShow(true);
+        // }
+    }
 
     return (
         <div>
-            <h2> Lista de presença</h2>
-            <ul>
-                {list.map((item, index) => (
-                    <Pessoa key={index} data= {item}/>
-                ))}
-            </ul>
+            <button onClick = {handleClick}> {show ? 'Ocultar' : 'Mostrar'} </button>
+
+            {show && 
+                <div>
+                    Blabla texto ipsum illori....
+                </div>
+            }
         </div>
     )
-
 }
 
 export default App;
 
-// Coloco meu componente pessoa para repetir as informações, só preciso passar data e o meu objeto item.
-// Atentar ao erro de key prop. Preciso colocar a prop key com index, mesmo que não receba a prop, pois ela serve para o map. E não ao compoenente.
+
+// Forma mais fácil: 
+// Set show inicia como falso.. o meu state.
+// Texto ser exibido se minha state estiver como true, preciso criar uma expressão que é uma verificação caso seja positivo.
+// Se show estiver true elem ostra blabla...
+// No btn irei colocar um handleClick de evento, preciso criar a fç anônima handleClick e irei colocar um setShow true.
+// Para exibir o blabla não fiz uma ação direta, eu criei um intermediário que é show , em que as duas coisas mexeram com ela. Quando show estiver true mostrar o blabla.
+
+// Toogle - executa uma vez faz algo, quando executo retorna ao estado de origem. Como fazer?
+// Usar condicional if(show) else... consigo resumir em uma linha invertida com setShow( !show )
+
+// Mudar o texto do botão. Condicional ternário junto com a var state.
+// Quando se tem uma condição faz da 1ª maneira, se for toogle faz 2ª a condicional com ternário.
